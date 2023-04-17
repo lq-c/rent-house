4ec753ff7ade8836ba71443bf165e0aae9f07e76
longqiang <longqiang.chen@cscad.com>
2023-04-17 18:19:57 +0800

    login-back

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,11 +3,20 @@
 </script>
 
 <template>
-    <div>
-        
+    <div class="login-back">
+
     </div>
 </template>
 
 <style scoped>
-
+.login-back{
+    width: 100%;
+    height: 100%;
+    background-image: url('../assets/login-back.jpg');
+    background-position: 100% 100%;
+    background-size: 100% 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+}
 </style>
